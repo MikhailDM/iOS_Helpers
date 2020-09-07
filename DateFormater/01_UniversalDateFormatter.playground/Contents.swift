cafa9039d0de782321a42c6adefaec2a8d0a89a1
Dmitriev Mikhail <misha.dmitriev.spb@gmail.com>
2020-09-07 10:27:24 +0300

    DateFormatter	. Add DataFromDate

--- DateFormater/01_UniversalDateFormatter.playground/Contents.swift
+++ DateFormater/01_UniversalDateFormatter.playground/Contents.swift
@@ -2,7 +2,7 @@ import Foundation
 
 //Работаем с датами
 
-//MARK: - INIT
+//MARK: - Init
 
 private let calendar = Calendar.current
 
@@ -10,11 +10,24 @@ enum DateFormat: String {
     case onlyDayOfWeek = "EEEEE"
     case onlyHoursAndMinutes = "HH:mm"
     case dateWithDash = "yyyy-MM-dd"
+    case onlyDay = "d"
 }
 
 enum DateLocale: String {
     case ru = "ru_RU"
     case en = "en_EN"
+}
+
+enum DateType {
+    case startOfTheWeek
+    case endOfTheWeek
+    case startOfTheMonth
+    case endOfTheMonth
+    case nextMonth
+    case previousMonth
+    case weekAgo
+    case startOfTheDay
+    case endOfTheDay
 }
 
 private func initDateFormatter(dateFormat: DateFormat, locale: DateLocale) -> DateFormatter {
@@ -25,47 +38,127 @@ private func initDateFormatter(dateFormat: DateFormat, locale: DateLocale) -> Da
 }
 
 
-//MARK: - FUNCTION. UNIVERSAL DATE FORMAT FUNCTION
+//MARK: - Custom data
 
+private let monthNamesLong = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"]
+private let monthNamesShort = ["янв", "фев", "мар", "апр", "май", "июн", "июл", "авг", "сен", "окт", "ноя", "дек"]
+
+
+
+
+//MARK: - Function. Universal Date formatter. Get String from Date
+
+/// Get new String from Date
+/// - Parameters:
+///   - date: Initial Date
+///   - format: New Date format
+///   - locale: Locale
+/// - Returns: String
 func getStringFromDate(date: Date, format: DateFormat, locale: DateLocale) -> String {
     let df = initDateFormatter(dateFormat: format, locale: locale)
     return df.string(from: date)
 }
 
 
+//MARK: - Function. Universal Date formatter. Get Date from Date
 
-
-//MARK: - FUNCTION. ONLY WEEKDAY IN STRING. "Вт"
-
-func onlyDayOfWeek(from date: Date) -> String {
-    let df = initDateFormatter(dateFormat: .onlyDayOfWeek, locale: .ru)
-    return df.string(from: date).capitalized
+/// Get new Date from Date
+/// - Parameters:
+///   - date: Initial Date
+///   - newType: New Date format
+/// - Returns: Date
+func getDateFromDate(from date: Date, newType: DateType) -> Date {
+    switch newType {
+    case .startOfTheWeek:
+        guard
+            let sunday = calendar.date(from: calendar.dateComponents([.yearForWeekOfYear, .weekOfYear], from: date)),
+            let monday = calendar.date(byAdding: DateComponents(day: 1), to: sunday) else { return Date() }
+        //Check if initial day is Sunday
+        guard
+            let weekday = calendar.dateComponents([.weekday], from: date).weekday,
+            weekday == 1,
+            let addWeekAgoToMonday = calendar.date(byAdding: DateComponents(day: -7), to: monday) else { return monday }
+        return addWeekAgoToMonday
+        
+    case .endOfTheWeek:
+        let startOfTheWeek = getDateFromDate(from: date, newType: .startOfTheWeek)
+        guard let sunday = calendar.date(byAdding: DateComponents(day: -1, weekOfYear: 1), to: startOfTheWeek) else { return Date() }
+        return sunday
+        
+    case .startOfTheMonth:
+        guard let date = calendar.date(from: calendar.dateComponents([.year, .month], from: date)) else { return Date() }
+        return date
+        
+    case .endOfTheMonth:
+        let startOfTheMonth = getDateFromDate(from: date, newType: .startOfTheMonth)
+        guard let date = calendar.date(byAdding: DateComponents(month: 1, day: -1), to: startOfTheMonth) else { return Date() }
+        return date
+        
+    case .nextMonth:
+        guard let nextMonth = calendar.date(byAdding: DateComponents(month: 1), to: date) else { return Date() }
+        return nextMonth
+       
+    case .previousMonth:
+        guard let previousMonth = calendar.date(byAdding: DateComponents(month: -1), to: date) else { return Date() }
+        return previousMonth
+        
+    case .weekAgo:
+        guard let oneWeekAgo = calendar.date(byAdding: .day, value: -7, to: date) else { return Date() }
+        return oneWeekAgo
+        
+    case .startOfTheDay:
+        let startOfTheDay = calendar.startOfDay(for: date)
+        return startOfTheDay
+        
+    case .endOfTheDay:
+        let startOfTheDay = calendar.startOfDay(for: date)
+        guard let endOfTheDay = calendar.date(byAdding: DateComponents(day: 1, second: -1), to: startOfTheDay) else { return Date() }
+        return endOfTheDay
+    }
 }
 
 
-//MARK: - FUNCTION. ONLY HOURS AND MINUTES. "22:00"
 
-func onlyHoursAndMinutes(from date: Date) -> String {
-    let df = initDateFormatter(dateFormat: .onlyHoursAndMinutes, locale: .ru)
-    return df.string(from: date)
+
+//MARK: - Function. Get array of Dates
+
+/// Get array of Dates
+/// - Parameters:
+///   - from: Start Date
+///   - to: End Date
+/// - Returns: [Date]
+func getDatesRange(from: Date, to: Date) -> [Date] {
+    // In case of the "from" date is more than "to" date it should returns an empty array
+    if from > to { return [Date]() }
+    var tempDate = from
+    var array = [tempDate]
+    while tempDate < to {
+        tempDate = Calendar.current.date(byAdding: .day, value: 1, to: tempDate)!
+        array.append(tempDate)
+    }
+    return array
 }
 
-//MARK: - FUNCTION. SIMPLE DATE. "2020-12-25"
 
-func dateWithDash(from date: Date) -> String {
-    let df = initDateFormatter(dateFormat: .dateWithDash, locale: .ru)
-    return df.string(from: date)
+//MARK: - Function. Custom Data
+
+/// Return custom data from Date
+/// - Parameter date: Date
+/// - Returns: "Январь 2020"/"Февраль 2020"...
+func getDateInLongFormatString(from date: Date) -> String {
+    let dateComponents = calendar.dateComponents([.month, .year], from: date)
+    guard
+        let dateMonthInt = dateComponents.month,
+        let dateYearInt = dateComponents.year else { return "" }
+    let dateYearString = "\(dateYearInt)"
+    let dateMonthString = monthNamesLong[dateMonthInt - 1]
+    let dateString = "\(dateMonthString) \(dateYearString)"
+    
+    return dateString
 }
 
-//MARK: - FUNCTION. DATE A WEEK AGO MORNING
 
-func getDateWeekAgo(from date: Date) -> Date {
-    guard var oneWeekAgoMorning = calendar.date(byAdding: .day, value: -6, to: date) else { return Date() }
-    oneWeekAgoMorning = calendar.startOfDay(for: oneWeekAgoMorning)
-    return oneWeekAgoMorning
-}
-
-//MARK: - FUNCTION. GET DATE COMPONENTS
+//MARK: - Function. Get Date components
 
 func getDateComponents(from date: Date) {
     let dcs = calendar.dateComponents([.year, .month, .day], from: date)
@@ -74,10 +167,7 @@ func getDateComponents(from date: Date) {
 }
 
 
+let sunday = calendar.date(byAdding: DateComponents(day: -1), to: Date())
+print(getDateFromDate(from: sunday!, newType: .startOfTheWeek))
+print(getDateFromDate(from: Date(), newType: .startOfTheWeek))
 
-print(onlyDayOfWeek(from: Date()))
-print(onlyHoursAndMinutes(from: Date()))
-print(dateWithDash(from: Date()))
-print(getDateWeekAgo(from: Date()))
-print(getDateComponents(from: Date()))
-print(getStringFromDate(date: Date(), format: .onlyHoursAndMinutes, locale: .ru))
