b6d5b3b0dd815241b3fff4000de5234c8c0917ba
Mikhail Dmitriev <misha.dmitriev.spb@gmail.com>
2021-01-28 12:48:28 +0300

    Template. ReusView

---
+++ _Templates/_Reusable View/Reusable View.xctemplate/___FILEBASENAME___ReusView.swift
@@ -0,0 +1,33 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___
+//
+
+import UIKit
+
+class ___VARIABLE_sceneName___ReusView: UIView {
+    
+    //MARK: - Outlets
+    
+    //MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        loadView()
+    }
+    
+    //MARK: - Load
+    func loadView() {
+        let bundle = Bundle(for: ___VARIABLE_sceneName___ReusView.self)
+        let nib = UINib(nibName: "___VARIABLE_sceneName___ReusView", bundle: bundle)
+        let view = nib.instantiate(withOwner: self).first as! UIView
+        view.frame = bounds
+        addSubview(view)
+    }
+}
