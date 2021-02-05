ac53d2b66bcd6386dfab491217f6da3fbe6ca71b
Mikhail Dmitriev <misha.dmitriev.spb@gmail.com>
2021-02-05 20:23:56 +0300

    Iterator. Generic

--- _Services/Iterator/UniversalIterator.playground/Contents.swift
+++ _Services/Iterator/UniversalIterator.playground/Contents.swift
@@ -1,38 +1,71 @@
 import Foundation
 
-protocol IteratorService {
-    init(input: [Int], membersCountToPassing: Int)
-    mutating func getNextMembers() -> [Int]
-}
-
-struct DataIterator: IteratorService {
+struct DataIterator<T: Equatable> {
     
     //MARK: - Properties
-    private var input: [Int]
+    private var input: [T]
     private var membersCountToPassing: Int
-    private var passedMembers = [Int]()
+    private var passedMembers = [T]()
     
     //MARK: - Init
-    init(input: [Int], membersCountToPassing: Int) {
+    init(input: [T], membersCountToPassing: Int) {
         self.input = input
         self.membersCountToPassing = membersCountToPassing
     }
     
-    mutating func getNextMembers() -> [Int] {
-        let remainMembers = input.filter { !passedMembers.contains($0) }
+    //MARK: - Logic
+    mutating func getNextMembers() -> [T] {
+        let remainMembers = input.filter { member in !passedMembers.contains(member) }
         let membersToPassing = Array(remainMembers.prefix(membersCountToPassing))
         passedMembers.append(contentsOf: membersToPassing)
         
         guard !membersToPassing.isEmpty else { return [] }
         return membersToPassing
     }
+    
+    mutating func clearPassedMembers() {
+        passedMembers = []
+    }
 }
 
+print("===== Iterator Int")
 let inputInt = [1,2,3,4,5,6,7]
+var iteratorInt = DataIterator(input: inputInt, membersCountToPassing: 2)
+print(iteratorInt.getNextMembers())
+print(iteratorInt.getNextMembers())
+print(iteratorInt.getNextMembers())
+print(iteratorInt.getNextMembers())
+print(iteratorInt.getNextMembers())
+iteratorInt.clearPassedMembers()
+print(iteratorInt.getNextMembers())
+
+
+print("===== Iterator String")
 let inputStr = ["A","B","C","D","E","F","G"]
-var iterator: IteratorService = DataIterator(input: inputInt, membersCountToPassing: 2)
-print(iterator.getNextMembers())
-print(iterator.getNextMembers())
-print(iterator.getNextMembers())
-print(iterator.getNextMembers())
-print(iterator.getNextMembers())
+var iteratorStr = DataIterator(input: inputStr, membersCountToPassing: 2)
+print(iteratorStr.getNextMembers())
+print(iteratorStr.getNextMembers())
+print(iteratorStr.getNextMembers())
+print(iteratorStr.getNextMembers())
+print(iteratorStr.getNextMembers())
+iteratorStr.clearPassedMembers()
+print(iteratorStr.getNextMembers())
+print(iteratorStr.getNextMembers())
+
+
+print("===== Iterator Models")
+struct SomeNewModel: Equatable, CustomStringConvertible {
+    var name: String
+    static func == (lhs: SomeNewModel, rhs: SomeNewModel) -> Bool {
+        return lhs.name == rhs.name
+    }
+    public var description: String {
+        return name
+    }
+}
+
+let inputModels = [SomeNewModel(name: "111"), SomeNewModel(name: "222"), SomeNewModel(name: "333"), SomeNewModel(name: "444")]
+var iteratorModels = DataIterator(input: inputModels, membersCountToPassing: 2)
+print(iteratorModels.getNextMembers())
+print(iteratorModels.getNextMembers())
+print(iteratorModels.getNextMembers())
