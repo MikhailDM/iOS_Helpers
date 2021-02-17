6f53d4474c0c59f8155b873517c0db58f0c45e17
Mikhail Dmitriev <misha.dmitriev.spb@gmail.com>
2021-02-17 16:13:22 +0300

    Animations

---
+++ _Projects/_Animations/Animations/Main/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  Animations
+//
+//  Created by Mikhail Dmitriev on 17.02.2021.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Main"
+    }
+}
